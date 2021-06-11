73a3b3653b1aeed3a0009e2f2cf9c27346d6deb0
alex <t17611739300@163.com>
2021-06-11 18:59:41 +0800

    v16

--- src/router/router.js
+++ src/router/router.js
@@ -11,6 +11,7 @@ import Params from '@/components/goods/Params'
 import List from '@/components/goods/List'
 import Add from '@/components/goods/Add'
 import Order from '@/components/order/Order'
+import Report from '@/components/report/Report'
 
 Vue.use(VueRouter)
 
@@ -63,6 +64,10 @@ const routes = [
       {
         path: '/orders',
         component: Order
+      },
+      {
+        path: '/reports',
+        component: Report
       },
     ]
   }
